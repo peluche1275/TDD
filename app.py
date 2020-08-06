cdcbd6710c425349e54072587869a1d52d55098c
Nathan <nathan.altomare.belier@gmail.com>
2020-08-06 19:35:13 +0200

    All tests are green

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 class StrConvertor:
+    
     def convert(string):
+
         dictio = {
-            "Zero": 0,
-            "Un": 1,
-            "Deux": 2,
-            "Trois": 3,
-            "Quatre": 4,
-            "Cinq": 5,
-            "Six": 6,
-            "Sept": 7,
-            "Huit": 8,
-            "Neuf": 9,
+            "Zero": "0",
+            "Un": "1",
+            "Deux": "2",
+            "Trois": "3",
+            "Quatre": "4",
+            "Cinq": "5",
+            "Six": "6",
+            "Sept": "7",
+            "Huit": "8",
+            "Neuf": "9",
             "Plus": "+",
             "Moins": "-"
         }
-        return dictio.get(string)
+
+        import re
+
+        convert = re.findall('[A-Z][^A-Z]*',string)
+        liste = []
+
+        for x in convert:
+            liste.append(dictio.get(x))
+        
+        return eval("".join(liste))
 
 #  TEST #
 
@@ -30,8 +41,6 @@ def testStringConvertorToInt():
     StringConvertor = StrConvertor
     print(assertEquals(StringConvertor.convert('Deux'), 2))
     print(assertEquals(StringConvertor.convert('Trois'), 3))
-    print(assertEquals(StringConvertor.convert('Plus'), "+"))
-    print(assertEquals(StringConvertor.convert('Moins'), "-"))
     print(assertEquals(StringConvertor.convert('DeuxPlusDeux'), 4))
     print(assertEquals(StringConvertor.convert('TroisMoinsDeux'), 1))
     print(assertEquals(StringConvertor.convert('TroisMoinsDeuxPlusCinqMoinsSix'), 0))

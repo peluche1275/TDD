dbd4fc267e417919a0f2b19e618e099e66960816
Nathan <nathan.altomare.belier@gmail.com>
2020-09-16 17:33:05 +0200

    clean up the code

--- descendingOrder.py
+++ descendingOrder.py
@@ -2,16 +2,28 @@ class descendingOrder:
     def __init__(self):
         pass
 
-    def descending(self,test):
-        testtdd = self.convertStringToInteger(self.split(test))
-        testtdd.sort(reverse=True)
-        strings = [str(integer) for integer in testtdd]
-        a_string = "".join(strings)
-        an_integer = int(a_string)
-        return an_integer
+    def descending(self,stringToConvertToInteger):
 
-    def split(self, test):
-        return [char for char in test]
+        stringConvertedToIntegerList = self.convertStringListToIntegerList(self.splitStringToList(stringToConvertToInteger))
 
-    def convertStringToInteger(self,test_list):
-        return list(map(int, test_list))
+        self.putInReverseOrder(stringConvertedToIntegerList)
+
+        integerListConvertedToStringList = [str(integer) for integer in stringConvertedToIntegerList]
+
+        stringListConvertedToString = "".join(integerListConvertedToStringList)
+
+        stringConvertedToInteger = int(stringListConvertedToString)
+
+        return stringConvertedToInteger
+
+    def putInReverseOrder(self,integerList):
+        
+        return integerList.sort(reverse=True)
+
+    def splitStringToList(self, stringToSplitIntoList):
+
+        return [char for char in stringToSplitIntoList]
+
+    def convertStringListToIntegerList(self,stringConvertedToList):
+
+        return list(map(int, stringConvertedToList))
